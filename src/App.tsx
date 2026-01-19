0f1879f347d6e4f49065eda9a0058ba06fe9582a
maya8624 <maya8624@hotmail.com>
2026-01-19 19:56:02 +1100

    add AuthGuard

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
+import React from "react";
 import { Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
-import ProductsPage from "./pages/ProductsPage"; // placeholder
+import ProductsPage from "./pages/ProductsPage";
+import CartPage from "./pages/CartPage";
+import CheckoutPage from "./pages/CheckoutPage";
+import AuthGuard from "./components/AuthGuard";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPage />} />
       <Route path="/" element={<ProductsPage />} />
+      <Route
+        path="/cart"
+        element={
+          <AuthGuard>
+            <CartPage />
+          </AuthGuard>
+        }
+      />
+      <Route
+        path="/checkout"
+        element={
+          <AuthGuard>
+            <CheckoutPage />
+          </AuthGuard>
+        }
+      />
     </Routes>
   );
 }
